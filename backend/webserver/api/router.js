ea54ca2d7e91453405c9621382a40797cfb1ea97
Aymane Khouaji <akhouaji@linagora.com>
2017-05-03 11:41:14 +0200

    Secure route in backend

--- backend/webserver/api/router.js
+++ backend/webserver/api/router.js
@@ -2,24 +2,24 @@
 
 
 module.exports = function(dependencies, lib, router) {
-
+  const authorizationMW = dependencies('authorizationMW');
   var fileController = require('../controllers/filesController')(dependencies, lib);
   var middleware = require('./middleware')(dependencies);
 
 
   //router.post('/convertion', controller.convertion);
 
-  router.get('/files/:fileId', fileController.existingFile);
+  router.get('/files/:fileId', authorizationMW.requiresAPILogin, fileController.existingFile);
 
-  router.post('/save/files/:fileId/:extFile', fileController.saveModif);
+  router.post('/save/files/:fileId/:extFile', authorizationMW.requiresAPILogin, fileController.saveModif);
 
-  router.post('/coauthor/files/:fileId', fileController.addCoAuthor);
+  router.post('/coauthor/files/:fileId', authorizationMW.requiresAPILogin, fileController.addCoAuthor);
 
-  router.post('/files', fileController.newFile);
+  router.post('/files', authorizationMW.requiresAPILogin, fileController.newFile);
 
-  router.get('/files', fileController.getMetaDataByUserId);
+  router.get('/files', authorizationMW.requiresAPILogin, fileController.getMetaDataByUserId);
 
-  router.delete('/files/:fileId', fileController.removeFile);
+  router.delete('/files/:fileId', authorizationMW.requiresAPILogin, fileController.removeFile);
 
   return router;
 };
