72aba17995329ae81fc1f13b1ff3b7a889774607
Aymane Khouaji <akhouaji@linagora.com>
2017-06-01 17:57:34 +0200

    Reduce the aggregate function + adapt the frontend

--- backend/lib/document.js
+++ backend/lib/document.js
@@ -4,38 +4,30 @@ module.exports = function(dependencies) {
 
   const mongoose = dependencies('db').mongo.mongoose;
   const documentModel = mongoose.model('document');
+  const CONSTANTS = require('./constants');
 
   function UpdateOrCreate(usersID, documentID, callback) {
     return documentModel.findOneAndUpdate({document: documentID}, {$set: {document: documentID}, $push: {users: { $each: usersID}}}, {new: true, upsert: true})
       .populate('document').exec(callback);
   }
 
-  function getDocumentsByUserID(userID, callback) {
-    return documentModel.aggregate({$match: {users: userID}})
-      .group({
-        _id: userID,
-        coAuthor: {$addToSet: '$users'},
-        documents: {$push: '$document'}
-      })
-      .lookup({
-        from: 'fs.files',
-        localField: 'documents',
-        foreignField: '_id',
-        as: 'documents'
-      })
-      .unwind('$documents')
-      .lookup({
-        from: 'users',
-        localField: 'documents.metadata.creator.id',
-        foreignField: '_id',
-        as: 'documents.metadata.creator'
-      })
-      .project('-documents.metadata.creator.password -documents.metadata.creator.accounts')
-      .group({
-        _id: userID,
-        documents: {$push: '$documents'}
-      })
-      .exec(callback);
+  function getDocumentsByUserID(userID, options = {}, callback) {
+    return documentModel.aggregate([
+      { $match: { users: userID } },
+      { $lookup: {
+          from: 'fs.files', localField: 'document', foreignField: '_id', as: 'documents2'
+      }},
+      { $project: {
+          _id: 1, users: 1, document: { $arrayElemAt: ['$documents2', 0] }
+      }},
+      { $sort: { 'document.uploadDate': -1 } },
+      { $group: {
+          _id: userID,
+          documents: { $push: '$document' }
+      }},
+      { $skip: +options.offset || CONSTANTS.DEFAULT_OFFSET },
+      { $limit: +options.limit || CONSTANTS.DEFAULT_LIMIT }
+    ], callback);
   }
 
   function remove(documentID, callback) {
