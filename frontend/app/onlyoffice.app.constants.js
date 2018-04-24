baa46811b006f8f61370beef3c2df1b06a07a663
Nicolas Parquet <nparquet@linagora.com>
2018-04-24 16:25:15 +0200

    Updated to latest widget version following update from nexedi

--- frontend/app/onlyoffice.app.constants.js
+++ frontend/app/onlyoffice.app.constants.js
@@ -2,5 +2,5 @@
   'use strict';
 
   angular.module('linagora.esn.onlyoffice')
-    .constant('EDITOR_WIDGET_URL', 'https://spreadsheet.app.officejs.com/3e0f507406/app/onlyoffice.gadget.html');
+    .constant('EDITOR_WIDGET_URL', 'https://spreadsheet.app.officejs.com/845a05255c/app/onlyoffice.gadget.html');
 })();
